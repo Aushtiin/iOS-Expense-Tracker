fdd6f1742ecd16fef15be398f24ed3ca7989c73b
aushtiin <aotangban@gmail.com>
2023-07-15 15:53:06 +0200

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Austin  Tangban on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
